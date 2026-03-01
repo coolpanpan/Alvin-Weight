a3a2a26cfd7478731a1edf62c237ac39d49505e8
coolpanpan <148878165+coolpanpan@users.noreply.github.com>
2026-03-01 12:05:48 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let weight = 0;
-
-const title = document.getElementById("title");
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-  weight++;
-  title.textContent = `林靖恩 的體重：${weight} kg`;
-});
