acfdd5bafb3af47eacc1db5d2da5d6bd70e9d7ee
coolpanpan <148878165+coolpanpan@users.noreply.github.com>
2026-03-01 21:21:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-let weight = 0;
+const BIN_ID = "69a43cc143b1c97be9a947d1"; 
+const API_KEY = "$2a$10$.Rysps8J0rt1vIbGV/ziMOUogso7dFLpSVGgLq2m3I3cqHESjCgca";
 
-const title = document.getElementById("title");
-const btn = document.getElementById("btn");
+const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
-btn.addEventListener("click", () => {
-  weight++;
-  title.textContent = `林靖恩 的體重：${weight} kg`;
+// 讀取雲端 weight
+async function loadWeight() {
+  const res = await fetch(BIN_URL, {
+    headers: {
+      "X-Master-Key": API_KEY
+    }
+  });
+
+  const data = await res.json();
+  const weight = data.record.weight;
+
+  document.getElementById("title").innerText =
+    `林靖恩 的體重：${weight} kg`;
+
+  return weight;
+}
+
+// 將 weight 寫回雲端
+async function saveWeight(newValue) {
+  await fetch(BIN_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Master-Key": API_KEY
+    },
+    body: JSON.stringify({ weight: newValue })
+  });
+}
+
+// 點按鈕 +1
+document.getElementById("btn").addEventListener("click", async () => {
+  let current = await loadWeight();
+  let updated = current + 1;
+  await saveWeight(updated);
+  await loadWeight();
 });
+
+// 初始載入
+loadWeight();
