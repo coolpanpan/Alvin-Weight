912f91965e740b94ddf9eca4509e01e821a21ce6
coolpanpan <148878165+coolpanpan@users.noreply.github.com>
2026-03-16 20:59:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-console.log("🔥 script.js 有被載入");
+let weight = 0;
 
-// 讀取體重
-function loadWeight() {
-  let weight = localStorage.getItem("weight");
-
-  if (weight === null) {
-    weight = 0;
-    localStorage.setItem("weight", weight);
-  }
-
+function add(){
+  weight++;
   document.getElementById("title").innerText =
-    `林靖恩 的體重：${weight} kg`;
-
-  return Number(weight);
+  "林靖恩 的體重：" + weight + " kg";
 }
-
-// 儲存體重
-function saveWeight(value) {
-  localStorage.setItem("weight", value);
-}
-
-// 按鈕
-document.getElementById("btn").addEventListener("click", () => {
-  let current = loadWeight();
-  current += 1;
-
-  saveWeight(current);
-  loadWeight();
-});
-
-// 初始化
-loadWeight();
